e6622a6ea1fc90d554a560c9ef0be35883979a60
Aaron Collier <aaronc@kentico.com>
2019-08-29 12:56:15 +0200

    Fix to link resolver

--- article.js
+++ article.js
@@ -1,10 +1,17 @@
+// Reload page on hash change
+const renderHash = () => {
+  window.history.go();
+};
+
+window.addEventListener("hashchange", renderHash, false);
+
+// Create article container
 const app = document.getElementById("app");
 const articleContainer = document.createElement("div");
 articleContainer.setAttribute("class", "article");
 app.appendChild(articleContainer);
 
 var articleSlug = location.hash.slice(1);
-console.log(articleSlug);
 
 var Kc = window["kenticoCloudDelivery"];
 
@@ -15,19 +22,27 @@ var deliveryClient = new Kc.DeliveryClient({
 deliveryClient
   .items()
   .type("article")
-  .equalsFilter("elements.url_pattern",articleSlug)
+  .equalsFilter("elements.url_pattern", articleSlug)
   .queryConfig({
-      urlSlugResolver: (link,context) => {
-          if (link.type == 'article') {
-              return { url: `article.html#${urlPattern}`}
-          }
-          return { url: 'unsupported-link'};
+    urlSlugResolver: (link, context) => {
+      // Link to article hash
+      if (link.type == "article") {
+        return { 
+          html: `article.html#${link.urlSlug}` 
+        };
       }
+      // For coffee links, redirect to React site
+      if (link.type == "coffee") {
+        return {
+          url: `article.html#${link.urlSlug}`
+        };
+      }
+      return { url: "unsupported-link" };
+    }
   })
   .toPromise()
   .then(response => {
     var article = response.items[0];
-    console.log(article)
 
     let headerImage = document.createElement("img");
     headerImage.setAttribute("class", "article-header");
